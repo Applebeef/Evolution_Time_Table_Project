06ed466fffba5f16c22043d254fe94dd3ae51313
Applebeef <58487936+Applebeef@users.noreply.github.com>
2021-07-17 19:55:44 +0300

    doStuff

--- ui/src/Main_ui.java
+++ ui/src/Main_ui.java
@@ -1,2 +1,10 @@
 public class Main_ui {
+    public static void main(String[] args) {
+        System.out.println("blabla");
+
+
+        Main_engine test = new Main_engine();
+        test.doStuff();
+    }
+
 }
