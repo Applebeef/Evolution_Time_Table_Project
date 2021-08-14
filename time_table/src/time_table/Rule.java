aacf51b5edd40c368ec24bba7dbb143d25d04d17
tall1 <talev142@gmail.com>
2021-08-14 18:27:45 +0300

    New fifths

--- time_table/src/time_table/Rule.java
+++ time_table/src/time_table/Rule.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 
 
 public enum Rule {
+    //TODO add new EX2 rule
     TEACHER_IS_HUMAN("TeacherIsHuman") {
         @Override
         public double test(TimeTableSolution timeTableSolution) {
