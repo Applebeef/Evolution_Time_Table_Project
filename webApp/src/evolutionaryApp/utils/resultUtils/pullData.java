a8dee317dcfbdbaa00f6fbb536dae0fa36094686
tall1 <talev142@gmail.com>
2021-09-24 23:48:45 +0300

    added GetAllResultsServlet

--- webApp/src/evolutionaryApp/utils/resultUtils/pullData.java
+++ webApp/src/evolutionaryApp/utils/resultUtils/pullData.java
@@ -1,11 +1,11 @@
 package evolutionaryApp.utils.resultUtils;
 
-public class pullData {
+public class PullData {
     Integer generation;
     Double fitness;
     Boolean isAlive;
 
-    public pullData(Integer generation, Double fitness, Boolean isAlive) {
+    public PullData(Integer generation, Double fitness, Boolean isAlive) {
         this.generation = generation;
         this.fitness = fitness;
         this.isAlive = isAlive;
